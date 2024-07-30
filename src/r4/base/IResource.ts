355d45e2167bf62610f75be139a317fcfad9e17e
Roberto Araneda Espinoza <robaraneda@gmail.com>
2024-07-30 09:46:54 -0400

    remove resourcetype for resource interface

--- src/r4/base/IResource.ts
+++ src/r4/base/IResource.ts
@@ -1,11 +1,7 @@
-
 import { IMeta } from '../datatypes/IMeta';
-import { ResourceEnum } from '../enums';
-import { ResourceType } from '../types';
 import { IElement } from './IElement';
 
 export interface IResource {
-  resourceType: ResourceEnum | ResourceType
   id?: number | string;
   meta?: IMeta;
   implicitRules?: string;
