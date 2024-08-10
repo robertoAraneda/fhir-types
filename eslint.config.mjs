b6e93ee27d74c03181e052709cb79b6c0ec0d10d
Roberto Araneda Espinoza <robaraneda@gmail.com>
2024-08-09 20:33:22 -0400

    chore: update version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,15 @@ export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.node }},
   ...tseslint.configs.recommended,
+  {
+    rules:{
+      //"semi": ["error", "always"],
+      //"quotes": ["error", "single"],
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      // to enforce using type for object type definitions, can be type or interface
+      "@typescript-eslint/consistent-type-definitions": ["error", "interface"]
+    }
+  }
 ];
