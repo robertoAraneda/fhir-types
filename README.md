341b9e1abd3642a4f532e848dfce307331d5b694
Roberto Araneda Espinoza <robaraneda@gmail.com>
2024-08-10 21:30:10 -0400

    docs: add readme badges

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-fhirtypes
+<h1 align="center" style="border-bottom: none;">FHIR Types</h1>
+<h3 align="center">Types for R4 version of FHIR. R5 is comming :P</h3>
+<p align="center">
+<!--
+  <a href="https://github.com/semantic-release/semantic-release/actions/workflows/test.yml">
+    <img alt="Build states" src="https://github.com/semantic-release/semantic-release/actions/workflows/test.yml/badge.svg">
+  </a>
+  -->
+
+  <img alt="GitHub Release" src="https://img.shields.io/github/v/release/robertoAraneda/fhir-types">
+  <img alt="GitHub Actions Workflow Status" src="https://img.shields.io/github/actions/workflow/status/robertoAraneda/fhir-types/publish.yml">
+  <img alt="NPM Downloads" src="https://img.shields.io/npm/dw/fhirtypes">
+
+</p>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,28 @@
-import globals from "globals";
 import tseslint from "typescript-eslint";
+import jestPlugin from 'eslint-plugin-jest';
+import eslint from "@eslint/js";
 
-
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
-  ...tseslint.configs.recommended,
+export default tseslint.config(
   {
-    rules:{
+    // config with just ignores is the replacement for `.eslintignore`
+    ignores: ['**/build/**', '**/dist/**', "**/node_modules/**", "**/coverage/**"],
+  },
+  {
+    files: ["**/*.{ts}"]
+  },
+  eslint.configs.recommended,
+  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      jest: jestPlugin,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
       //"semi": ["error", "always"],
       //"quotes": ["error", "single"],
       "@typescript-eslint/no-unused-vars": "off",
@@ -15,6 +30,16 @@ export default [
       "@typescript-eslint/no-empty-object-type": "off",
       // to enforce using type for object type definitions, can be type or interface
       "@typescript-eslint/consistent-type-definitions": ["error", "interface"]
-    }
-  }
-];
+    },
+  },
+  {
+    // disable type-aware linting on JS files
+    files: ["**/*.{js}"],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    // enable jest rules on test files
+    files: ['test/**'],
+    ...jestPlugin.configs['flat/recommended'],
+  },
+);
