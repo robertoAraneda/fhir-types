71a3d3441e47f39f917263e050ee713acb6878a1
Roberto Araneda Espinoza <robaraneda@gmail.com>
2024-08-25 21:47:50 -0400

    fix: add optional field in coverage class

--- src/r4/backbones/ICoverageClass.ts
+++ src/r4/backbones/ICoverageClass.ts
@@ -1,31 +1,30 @@
-import { IBackboneElement, IElement } from "../base";
-import { ICodeableConcept } from "../datatypes";
+import { IBackboneElement, IElement } from '../base';
+import { ICodeableConcept } from '../datatypes';
 
 export interface ICoverageClass extends IBackboneElement {
+  /**
+   * @description The type of classification for which an insurer-specific class label or number and optional name is provided, for example may be used to identify a class of coverage or employer group, Policy, Plan.
+   */
+  type: ICodeableConcept;
 
-    /**
-     * @description The type of classification for which an insurer-specific class label or number and optional name is provided, for example may be used to identify a class of coverage or employer group, Policy, Plan.
-     */
-    type: ICodeableConcept;
+  /**
+   * @description The alphanumeric string value associated with the insurer issued label.
+   */
+  value: string;
 
-    /**
-     * @description The alphanumeric string value associated with the insurer issued label.
-     */
-    value: string;
+  /**
+   * @description A short description for the class.
+   */
+  name?: string;
 
-    /**
-     * @description A short description for the class.
-     */
-    name?: string;
+  // Extensions attributes
+  /**
+   * @description Extension for value
+   */
+  _value?: IElement;
 
-    // Extensions attributes
-    /**
-     * @description Extension for value
-     */
-    _value: IElement;
-
-    /**
-     * @description Extension for name
-     */
-    _name?: IElement;
+  /**
+   * @description Extension for name
+   */
+  _name?: IElement;
 }
